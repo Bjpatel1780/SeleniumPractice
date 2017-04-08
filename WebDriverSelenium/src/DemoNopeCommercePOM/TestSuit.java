5cb466e025af64b4c8716ccb95cf980198568b6b
bhavesh1780 <bhavesh1780@gmail.com>
2017-04-08 09:14:47 +0100

    First commit for Selenium

---
+++ WebDriverSelenium/src/DemoNopeCommercePOM/TestSuit.java
@@ -0,0 +1,69 @@
+package DemoNopeCommercePOM;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+
+/**
+ * Created by Dell on 01/04/2017.
+ */
+public class TestSuit extends BasePage {
+
+    Homepage homepage = new Homepage();
+    RegistrationPage registrationPage = new RegistrationPage();
+    LogInPage logInPage = new LogInPage();
+    BookPage bookPage = new BookPage();
+    CartPage cartPage = new CartPage();
+    CheckOutPage checkOutPage = new CheckOutPage();
+
+    @Test
+    public void userShouldAbleToRegisterSuccessfully(){
+        openBrowser();
+        homepage.clickOnRegisterButton();
+        registrationPage.registerNewUSer();
+        Assert.assertEquals("Your registration completed", Utils.getTextFromElement(By.className("result")) );
+        closeBrowser();
+    }
+
+    @Test
+    public void userShouldBeAbleToLogInSuccessfully(){
+        openBrowser();
+        homepage.clickOnLogInButton();
+        logInPage.login("xyz0256170902@gmail.com",  "Patel1");
+        Assert.assertTrue(Utils.checkIfElementIsDisplayed(By.linkText("Log out")));
+        closeBrowser();
+
+    }
+
+    @Test
+    public void userShouldLogOutSuccessfully(){
+        openBrowser();
+        homepage.clickOnLogInButton();
+        logInPage.login("xyz0217171026@gmail.com",  "Patel1");
+        homepage.clickOnLogOutButton();
+        Assert.assertTrue(Utils.checkIfElementIsDisplayed(By.linkText("Log in")));
+        closeBrowser();
+
+    }
+
+    @Test
+    public void userShouldAbleToAddProductSuccessfully(){
+        openBrowser();
+        bookPage.clickOnBookPageMenu();
+        Assert.assertTrue(Utils.checkIfElementIsDisplayed(By.xpath("//div[@class='master-wrapper-content']/div/div/div/div/h1")));
+        closeBrowser();
+
+    }
+
+    @Test
+    public void userShouldAbleToPurchaseProductSuccessfullyAsAGuest(){
+        openBrowser();
+        bookPage.clickOnBookPageMenu();
+        cartPage.checkOutShoppingCart();
+        cartPage.clickOnCheckOutAsAGuest();
+        checkOutPage.checkOutAsAGuest();
+
+    }
+
+
+}
